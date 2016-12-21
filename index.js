9d1c8f98be104ba8edd39dce4b5c93c2814ad355
Zhenting <zhentzhou@gmail.com>
2016-12-20 22:45:36 -0500

    basic

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+// app.use(bodyParser.json());
+app.get('/', (req,res)=>{
+  res.send("Hello World!");
+})
+
+const port = 3000;
+app.listen(port, ()=>{console.log("listenin on port:"+port+" captain!")});
